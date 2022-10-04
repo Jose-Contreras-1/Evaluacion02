11e76b50332e3afcac50621912766cef5c01cff7
Jose-Contreras-1 <102198153+Jose-Contreras-1@users.noreply.github.com>
2022-10-04 01:30:04 -0300

    final commit

---
+++ target/evaluacion02-1.0-SNAPSHOT/final.jsp
@@ -0,0 +1,15 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<html>
+    <head>
+        <meta http-equiv="content-Type" content="text/html; charset=UTF-8">
+        <title>JSP PAGE</title>
+    </head>
+    <body>
+<center>
+        <h1>Ingreso Exitoso!</h1>
+
+        <a href="index.jsp">Volver a Ingresar</a>
+</center>
+</body>
+</html>
---
+++ target/generated-sources/annotations/entities/Comic_.java
@@ -0,0 +1,18 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2022-10-02T23:04:00")
+@StaticMetamodel(Comic.class)
+public class Comic_ { 
+
+    public static volatile SingularAttribute<Comic, String> editorial;
+    public static volatile SingularAttribute<Comic, String> editorialPublicacion;
+    public static volatile SingularAttribute<Comic, Integer> numeroDeLibro;
+    public static volatile SingularAttribute<Comic, String> escritoIlustrador;
+    public static volatile SingularAttribute<Comic, Integer> id;
+    public static volatile SingularAttribute<Comic, String> nombre;
+
+}
